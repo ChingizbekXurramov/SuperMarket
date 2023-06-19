cc93c4d420c292cb6aa62471726bffca4e3c90fa
Chingizbek <128830130+ChingizbekXurramov@users.noreply.github.com>
2023-06-19 12:50:25 +0500

    SuperMarket

---
+++ SMArket/SMArket/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMArket
+{
+    internal class Employee
+    {
+        private string name;
+        private string position;
+        private double salary;
+
+        public Employee(string name, string position, double salary)
+        {
+            this.name = name;
+            this.position = position;
+            this.salary = salary;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public override string ToString()
+        {
+            return name + " - " + position + " - $" + salary;
+        }
+    }
+}
